24e0254979c37dab7ca081a92efa7b6a4a1b1863
rafal <rafalposwiata@gmail.com>
2020-01-16 22:10:19 +0100

    run sentiment_analysis

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 example_dir=$1
 
-python label_studio/server.py \
-    -p ${PORT:-8200} \
-    -i examples/$example_dir/tasks.json \
-    -o examples/$example_dir/completions \
-    -l examples/$example_dir/config.xml
+python label_studio/server.py init --template=sentiment_analysis sentiment_analysis_project
+python label_studio/server.py start sentiment_analysis_project
