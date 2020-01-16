706bd6ffe10b304096e6af1866f051c3b3427a45
rafal <rafalposwiata@gmail.com>
2020-01-16 22:24:16 +0100

    port assigning

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -2,4 +2,4 @@
 example_dir=$1
 
 python label_studio/server.py init --template=sentiment_analysis sentiment_analysis_project
-python label_studio/server.py start sentiment_analysis_project
+python label_studio/server.py -p ${PORT:-8200} start sentiment_analysis_project
